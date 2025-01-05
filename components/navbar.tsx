6ee9678b8bc707acf1ced3885a0834ab1fe6fd6e
Dimitris Xenias <xenias@thinkdesquared.com>
2025-01-05 18:34:01 +0200

    feat: ready the quiz

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -70,7 +70,7 @@ export function Logo() {
   return (
     <Link href='/' className='flex items-center gap-2.5'>
       <CommandIcon className='w-6 h-6 text-muted-foreground' strokeWidth={2} />
-      <h2 className='text-md font-bold font-code'>OCT School</h2>
+      <h2 className='text-md font-bold font-code'>OCT Paper</h2>
     </Link>
   );
 }
